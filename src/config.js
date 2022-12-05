4e3970a5a903d59a420d1363957d3cb5f2191973
pancevac <40486566+pancevac@users.noreply.github.com>
2022-12-05 13:07:59 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: 'https://ytcs-service.herokuapp.com/api/v1',
+  apiUrl: 'https://youtube-proxy-service.onrender.com/api/v1',
   apiKey: '6bfe90c0a6e7ec8cfbab4738f29c5215'
 }
 
